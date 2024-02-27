1f0c8e321151dc5d5ce4959440f24536a34fd86d
Emanuil <eiv.razgrad@gmail.com>
2024-02-27 21:09:38 +0200

    add

---
+++ src/test/java/com/example/Homework_Managment_Recipe_Products/Controller/ProductControllerIntegrationTest.java
@@ -0,0 +1,38 @@
+package com.example.Homework_Managment_Recipe_Products.Controller;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductControllerIntegrationTest {
+
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnListProducts() throws Exception {
+        this.mockMvc.perform(get("/product"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("<h1>Списък с продукти</h1>")));
+    }
+
+
+
+}
